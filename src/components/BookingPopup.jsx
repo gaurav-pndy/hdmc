e13bac661ceb44546920c65ae39a73f6445dfe56
gaurav-pndy <gauravpndy00@gmail.com>
2025-11-17 17:59:56 +0530

    updated popup

--- src/components/BookingPopup.jsx
+++ src/components/BookingPopup.jsx
@@ -12,18 +12,18 @@ const BookingPopup = ({ show, onClose }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm">
       {/* Popup Container */}
-      <div className="bg-white rounded-xl w-[95%] max-w-3xl overflow-hidden shadow-2xl relative animate-[popup_0.25s_ease]">
+      <div className="bg-white rounded-xl w-[95%] max-w-3xl overflow-hidden shadow-2xl pt-12 relative animate-[popup_0.25s_ease]">
         {/* Close Button */}
         <button
           onClick={onClose}
-          className="absolute top-3 right-3 bg-gray-100 hover:bg-gray-200 p-2 rounded-full"
+          className="absolute top-3 right-3 cursor-pointer  rounded-full"
         >
           <IoClose size={24} />
         </button>
 
         {/* Iframe */}
         <iframe
-          src="https://b22284.oz.1denta.ru/company/22284/menu?o="
+          src="https://1dn.su/5SC"
           className="w-full h-[70vh] md:h-[80vh]"
           //   loading="lazy"
         />
